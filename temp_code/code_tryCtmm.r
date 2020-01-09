a413958b850e1c7055fd2677c0d7300fbfdfe096
pratikunterwegs <pratikgupte16@gmail.com>
2020-01-09 23:23:17 +0100

    try ctmm on full data

--- temp_code/code_tryCtmm.r
+++ temp_code/code_tryCtmm.r
@@ -6,31 +6,54 @@ library(ctmm)
 library(fasttime)
 library(tibble)
 library(dplyr)
+library(purrr)
+library(tidyr)
+# devtools::install_github("pratikunterwegs/watlasUtils", ref = "devbranch")
+library(watlasUtils)
+library(lubridate)
+library(sf)
 
 # load some data
-data <- fread("data/435_024_revisit.csv")
-data <- data[,.(id, ts, x, y)]
-data[,ts:=fastPOSIXct(ts)]
+{
+  data <- fread("data/whole_season_tx_435.csv")
 
-# convert to lat-long
-library(sf)
-coords <- setDF(data) %>% 
-  st_as_sf(coords = c("x", "y")) %>% 
-  `st_crs<-`(32631) %>% 
-  st_transform(4326) %>% 
-  st_coordinates()
+  data <- funcCleanData(data)
+  
+  # setnames(data, old="ts", new="timestamp")
+}
 
-names(coords) <- c("x","y")
-
-data <- setDT(data)
-data[,`:=`(x=coords[,1], y=coords[,2])]
-
-setnames(data, c("individual.local.identifier", 
-                 "timestamp", "location.long", "location.lat"))
+# prepare for telemetry
+{
+  test = data
+  # convert to lat-long
+  coords <- test %>% 
+    st_as_sf(coords = c("x", "y")) %>% 
+    `st_crs<-`(32631) %>% 
+    st_transform(4326) %>% 
+    st_coordinates()
+  
+  names(coords) <- c("location.long","location.lat")
+  
+  test <- test[,.(id, ts)]
+  test[,ts:=fastPOSIXct(ts)]
+  setnames(test, c("individual.local.identifier", "timestamp"))
+  test[,`:=`(location.long = coords[,1], location.lat = coords[,2])]
+}
 
 # run ctmm proc
-data.tel <- as.telemetry(setDF(data))
-mod <- ctmm.fit(data.tel, CTMM = ctmm(tau = c(1)))
+{
+  data.tel <- as.telemetry(test)
+  
+  # make variogram
+  vg <- variogram(data.tel)
+  variogram.fit(vg)
+  
+  guess <- ctmm.guess(data.tel,variogram = vg, interactive = FALSE )
+  guess$error=TRUE
+  
+  mod = ctmm.fit(data.tel, CTMM=guess)
+}
 
 # plot to check
 plot(vg, CTMM = mod)
+  
