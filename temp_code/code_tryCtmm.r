fd450ec2e8d95705a7497417f6e50b966ada7847
pratik <pratikgupte16@gmail.com>
2020-01-10 14:36:00 +0100

    add calibration trial

--- temp_code/code_tryCtmm.r
+++ temp_code/code_tryCtmm.r
@@ -13,10 +13,15 @@ library(watlasUtils)
 library(lubridate)
 library(sf)
 
+{
+  griend <- st_read("data/griend_polygon/griend_polygon.shp")
+}
+
 # load some data
 {
   # read data and attractors
   data <- fread("data/whole_season_tx_435.csv", integer64 = "numeric")
+  
   attractors <- fread("data/attractor_points.txt")
   
   data <- wat_rmAttractor(df = data, 
@@ -41,27 +46,45 @@ library(sf)
   
   names(coords) <- c("location.long","location.lat")
   
-  test <- test[,.(id, ts)]
+  test[,HDOP:=sqrt(VARX+VARY)]
+  test <- test[,.(id, ts, HDOP)]
   test[,ts:=fastPOSIXct(ts)]
-  setnames(test, c("individual.local.identifier", "timestamp"))
+  setnames(test, c("individual.local.identifier", "timestamp", "HDOP"))
   test[,`:=`(location.long = coords[,1], location.lat = coords[,2])]
+  
+  prerel <- test[timestamp > "2018-08-16" & timestamp <= "2018-08-18",]
+  # prerel <- st_as_sf(prerel, coords = c("location.long", "location.lat")) %>% 
+  #   `st_crs<-`(4326) %>% 
+  #   st_transform(32631)
 }
 
-# run ctmm proc
+
+
+# make telemetry
 {
-  data.tel <- as.telemetry(test)
-  
+  data.tel <- as.telemetry(test[timestamp > "2018-08-18",])
+  data.cal <- as.telemetry(prerel)
+}
+
+# uere
+{
+  uere <- uere.fit(data.cal)
+  uere(data.tel) <- uere
+}
+
+# ctmm
+{
   outliers <- outlie(data.tel)
-  data.tel <- data.tel[-(which(outliers[[1]] >= 0.6)),]
+  data.tel <- data.tel[-(which(outliers[[1]] >= 0.07)),]
   
   # make variogram
   vg <- variogram(data.tel)
   variogram.fit(vg)
   
-  guess <- ctmm.guess(data.tel,variogram = vg, interactive = FALSE )
+  guess <- GUESS
   guess$error=TRUE
   
-  mod = ctmm.fit(data.tel, CTMM=guess)
+  mod = ctmm.fit(data.tel, CTMM=GUESS)
 }
 
 # plot to check
@@ -69,5 +92,19 @@ plot(vg, CTMM = mod)
 
 # get speeds
 data_speed <- speeds(data.tel, mod)
+setDT(data_speed)
+data_speed[,distance:=est*c(NA, diff(t))]
 
-hist(data_speed$est)
+# plot trajectories with speeds
+data2 <- test %>% 
+  inner_join(data_speed, by = "timestamp")
+
+# convert to spatial lines and points and export
+data2 <- st_as_sf(data2, coords = c("location.long", "location.lat"))
+st_write(data2, dsn = "data/points", layer = "ctmm_points", driver='ESRI Shapefile')
+
+lines <- st_coordinates(data2) %>% st_linestring() %>% st_sfc()
+data_lines <- data2 %>% 
+  select(t, est) %>% 
+  mutate(geometry = lines)
+st_write(data_lines, dsn = "data/lines", layer = "ctmm_lines", driver='ESRI Shapefile')
