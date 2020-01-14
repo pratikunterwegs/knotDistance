b581884d9eb7879b3022b598d981383e993e9c76
pratik <pratikgupte16@gmail.com>
2020-01-14 16:43:03 +0100

    update jobmaker

--- code/code_makeJobs.r
+++ code/code_makeJobs.r
@@ -8,20 +8,21 @@ writeLines(shebang, con="code/overall_job.sh")
 
 datafiles <- list.files("data/", pattern = "whole_season")
 
+# **BETTER JOB NAME**
 
 for (i in length(datafiles)) {
   
   id <- str_pad(i, 3, pad = "0")
   {
-    src <- glue('source("code/do_ctmm.r")')
-    text <- glue('do_ctmm("{datafiles[i]}")')
+    src <- glue('source("code/code_doCtmm.r")')
+    text <- glue('do_ctmm("data/{datafiles[i]}")')
     rfile <- glue('code/script_{id}.R')
     writeLines(c(src, text), con=rfile)
   }
   
   {
     
-    job <- glue('code/Rscript {rfile}')
+    job <- glue('Rscript {rfile}')
     jobfile <- glue('code/job_{id}.sh')
     writeLines(c(shebang, job), con=jobfile)
   }
