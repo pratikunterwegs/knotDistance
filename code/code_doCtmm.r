e0a1ca8ca6e046a0cfab7b5827d50ab1524f0f15
pratik_work <pratikgupte16@gmail.com>
2020-01-20 14:18:32 +0100

    correct source ctmm

--- code/code_doCtmm.r
+++ code/code_doCtmm.r
@@ -1,7 +1,9 @@
 #### code to do ctmm on file in cli args
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-source("code/code_doCtmm.r")
+source("code/code_funcCtmm.r")
 do_ctmm(args)
+
+message(paste("ctmm done on ", args))
 
 # ends here
