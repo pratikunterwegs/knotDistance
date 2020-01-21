76abec9c0584902ff0a19dd3428d6078ad5e6e3c
pratik_work <pratikgupte16@gmail.com>
2020-01-21 15:02:07 +0100

    rename ctmm funcs in do job

--- code/code_doCtmmTent.r
+++ code/code_doCtmmTent.r
@@ -1,7 +1,7 @@
 #### code to do ctmm on file in cli args
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
-source("code/code_funcCtmmTent.r")
+source("code/func_Ctmm_tent.r")
 do_ctmm_tent(args)
 
 message(paste("ctmm tent done on ", args))
