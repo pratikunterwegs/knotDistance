c11a6e75257b77af5ba5bf4f5d458c12d64ac03d
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-16 12:06:46 +0100

    convert args 2 to numeric

--- code/code_test_ctmm_scale.r
+++ code/code_test_ctmm_scale.r
@@ -1,11 +1,11 @@
 #### code to do ctmm on file in cli args
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=FALSE)
+args = commandArgs(trailingOnly=TRUE)
 source("code/func_test_ctmm_scale.r")
 
 message(glue::glue('args 1 = {args[1]} args 2 = {args[2]}'))
 
-test_ctmm_scale(datafile=args[1], scale=args[2])
+test_ctmm_scale(datafile=args[1], scale=as.numeric(args[2]))
 
 message(paste("ctmm done on ", args[1], " at scale ", args[2]))
 
