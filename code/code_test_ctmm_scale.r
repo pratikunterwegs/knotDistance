a89a6c95fe2d63c51ea91cf82c77d59fb308d736
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-16 10:24:45 +0100

    better args func

--- code/code_test_ctmm_scale.r
+++ code/code_test_ctmm_scale.r
@@ -2,8 +2,8 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=FALSE)
 source("code/func_test_ctmm_scale.r")
-test_ctmm_scale(args)
+test_ctmm_scale(datafile=args[1], scale=args[2])
 
-message(paste("ctmm done on ", args))
+message(paste("ctmm done on ", args[1], " at scale ", args[2]))
 
 # ends here
