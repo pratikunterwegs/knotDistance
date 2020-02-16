0fde3370446dfbf503a95a983547d97135641e02
pratikunterwegs <pratikgupte16@gmail.com>
2020-02-16 11:13:59 +0100

    print cli args

--- code/code_test_ctmm_scale.r
+++ code/code_test_ctmm_scale.r
@@ -2,6 +2,9 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=FALSE)
 source("code/func_test_ctmm_scale.r")
+
+message(glue::glue('args 1 = {args[1]} args 2 = {args[2]}'))
+
 test_ctmm_scale(datafile=args[1], scale=args[2])
 
 message(paste("ctmm done on ", args[1], " at scale ", args[2]))
